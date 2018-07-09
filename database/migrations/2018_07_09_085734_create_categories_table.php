595d1735240b1fc94e25b06d1b9c403f7c90310c
vohminhkhanh <vohminhkhanh@gmail.com>
2018-07-09 15:31:39 +0700

    update migration

--- database/migrations/2018_07_09_085734_create_categories_table.php
+++ database/migrations/2018_07_09_085734_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSummariesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateSummariesTable extends Migration
      */
     public function up()
     {
-        Schema::create('summaries', function (Blueprint $table){
-            $table->increments('id')->unsigned();
-            $table->integer('order_id')->unsigned();
-            $table->integer('cart_id')->unsigned();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateSummariesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('summaries');
+        Schema::dropIfExists('categories');
     }
 }
--- database/migrations/2018_07_09_090044_create_summaries_table.php
+++ database/migrations/2018_07_09_090044_create_summaries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateSummariesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('summaries', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->string('name');
+            $table->integer('order_id')->unsigned();
+            $table->integer('cart_id')->unsigned();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('summaries');
     }
 }
