ab809aa46ba06cea9ca77368374508d06fa311f1
vohminhkhanh <vohminhkhanh@gmail.com>
2018-07-09 14:13:09 +0700

    migration

--- database/migrations/2018_07_09_080823_create_users_table.php
+++ database/migrations/2018_07_09_080823_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->date('date_of_birth');
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            //
         });
     }
 
@@ -34,6 +35,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
